8380bf744af41582f432c7007f372487f02e9408
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2024-01-25 14:48:13 +0300

    feat: solve toStringList task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -207,9 +207,7 @@ const doubleArray = (arr) => arr.concat(arr);
  *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
  *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
-}
+const toStringList = (arr) => arr.toString();
 
 /**
  * Returns array containing only unique values from the specified array.
