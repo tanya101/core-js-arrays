26fc286620efec40fb41b0024872a4e6af9ac8a2
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2024-01-25 18:32:47 +0300

    feat: solve selectMany task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -266,9 +266,7 @@ const flattenArray = (nestedArray) => nestedArray.flat(Infinity);
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
-}
+const selectMany = (arr, childrenSelector) => arr.flatMap(childrenSelector);
 
 /**
  * Every month, you record your income and expenses.
