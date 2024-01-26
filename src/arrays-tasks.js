a70c42a9548c8adecdf604fc6864ab6c3c193d91
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2024-01-27 00:09:07 +0300

    feat: solve propagateItemsByPositionInde task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -454,9 +454,8 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
-}
+const propagateItemsByPositionIndex = (arr) =>
+  arr.flatMap((value, index) => Array(index + 1).fill(value));
 
 /**
  * Shifts an array by n positions. If n is negative, the array is shifted to the left;
