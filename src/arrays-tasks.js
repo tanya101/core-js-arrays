89a6350987a44ae14a51b9256b7caa7ae834a060
Tatiana Spiridonova <tn.spiridonova@yandex.ru>
2024-01-26 23:40:11 +0300

    feat: solve getMaxItems task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -408,9 +408,7 @@ const getHexRGBValues = (arr) =>
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
-}
+const getMaxItems = (arr, n) => arr.sort((a, b) => b - a).slice(0, n);
 
 /**
  * Finds and returns an array containing only the common elements found in two arrays.
